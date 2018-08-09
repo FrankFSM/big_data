61d166b3c5bdb15686fbbdb1415ec88ede04e2d4
haojixiang <hao_jixiang@163.com>
2018-08-09 22:39:44 +0800

    普通lock 中断lock 读写 lock

---
+++ src/main/java/com/ralap/bigdata/lock/LockDemo.java
@@ -0,0 +1,102 @@
+package com.ralap.bigdata.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
+/**
+ * @author: ralap
+ * @date: created at 2018/8/9 21:36
+ */
+public class LockDemo {
+
+
+    Lock lock = new ReentrantLock();
+
+    public void lock() {
+
+        new Thread(() -> {
+            boolean b = lock.tryLock();
+            System.out.println(b);
+            if (b) {
+                try {
+//                lock.lock();
+                    System.out.println(Thread.currentThread().getName() + "---->start");
+                    Thread.sleep(2000);
+                    System.out.println(Thread.currentThread().getName() + "---->finish");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }).start();
+
+    }
+
+    public Thread InterruptiblyLock() {
+
+        Thread t = new Thread(() -> {
+
+            try {
+                lock.lockInterruptibly();
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + "---->被中断");
+                return;
+            }
+            try {
+                System.out.println(Thread.currentThread().getName() + "---->start");
+                Thread.sleep(10000);
+                System.out.println(Thread.currentThread().getName() + "---->finish");
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+        return t;
+    }
+
+    ReadWriteLock rwLock = new ReentrantReadWriteLock();
+
+    public void rWLock() {
+
+       new Thread(() -> {
+            read(Thread.currentThread());
+            write(Thread.currentThread());
+        }).start();
+
+    }
+
+    public void read(Thread thread) {
+        rwLock.readLock().lock();
+        try {
+            long millis = System.currentTimeMillis();
+            while (System.currentTimeMillis() - millis <=1){
+                System.out.println(thread.getName() + "reading");
+            }
+            System.out.println(thread.getName() + "read finish");
+
+        } finally {
+            rwLock.readLock().unlock();
+        }
+    }
+
+    public void write(Thread thread) {
+        rwLock.writeLock().lock();
+        try {
+            long millis = System.currentTimeMillis();
+            while (System.currentTimeMillis() - millis <=1){
+                System.out.println(thread.getName() + "writeing");
+            }
+            System.out.println(thread.getName() + "write finish");
+
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+    }
+}
--- src/main/java/com/ralap/bigdata/lock/Main.java
+++ src/main/java/com/ralap/bigdata/lock/Main.java
@@ -1,5 +1,7 @@
 package com.ralap.bigdata.lock;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author: ralap
  * @date: created at 2018/8/8 22:13
@@ -9,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 2; i++) {
             int finalI = i;
-            new Thread(()->{
+            new Thread(() -> {
                 SyncDemo demo = new SyncDemo();
                 try {
                     demo.sy("Demo" + finalI);
@@ -21,5 +23,42 @@ public class Main {
         }
 
 
+    }
+
+    @Test
+    public void lockDemo() throws InterruptedException {
+
+        LockDemo demo = new LockDemo();
+        demo.lock();
+        demo.lock();
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void InterruptiblyLockDemo() {
+
+        LockDemo demo = new LockDemo();
+        Thread thread = demo.InterruptiblyLock();
+        Thread thread2 = demo.InterruptiblyLock();
+        thread.start();
+        thread2.start();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.interrupt();
+    }
+
+    @Test
+    public void RWLockDemo() {
+        LockDemo lockDemo = new LockDemo();
+        lockDemo.rWLock();
+        lockDemo.rWLock();
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
